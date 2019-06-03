0913f4e56ec66b14bf3e3ccbd8620e6735ba3b26
Bohodar <bodik264478@gmail.com>
2019-06-03 21:27:16 +0300

    Created react-class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <main className="App">
-      <h1>Hello World!!</h1>
-    </main>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+      title: 'TITLE'
+    };
+  }
+
+  render() {
+    return (
+      <main className="App">
+        <h1>
+          { this.state.title } - { this.state.count }
+        </h1>
+
+        <input
+          type="text"
+          value={this.state.title}
+          onChange={(event) => {
+            const lastChar = event.target.value.slice(-1);
+
+            if (Number.isInteger(+lastChar)) {
+              return;
+            }
+
+            this.setState({
+              title: event.target.value,
+            });
+          }}
+        />
+
+        <button onClick={() => {
+          this.setState({ count: this.state.count + 1 })
+        }}>
+          Add
+        </button>
+      </main>
+    );
+  }
 }
 
 export default App;
